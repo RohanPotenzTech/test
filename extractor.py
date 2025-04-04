8b5ae31bcb3d49d2d2a257bbcf3c485c005dda4b
RohanPotenzTech <rohan@potenztech.com>
2025-04-04 18:04:42 +0530

    Add files via upload

--- extractor.py
+++ extractor.py
@@ -1,14 +1,24 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
+import re
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
+
+ner_pipeline = pipeline("ner", model="dslim/bert-base-NER", grouped_entities=True)
 class Extractor:
+    def __init__(self):
+        model_name = "dslim/bert-base-NER"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForTokenClassification.from_pretrained(model_name)
+        self.ner_pipeline = pipeline("ner", model=self.model, tokenizer=self.tokenizer, aggregation_strategy="simple")  
+
     @staticmethod
     def extract_links(html, base_url):
         """Extracts all internal links from a webpage."""
         soup = BeautifulSoup(html, "html.parser")
         links = set()
 
-        for a_tag in soup.find_all("a", href=True):
+        for a_tag in soup.find_all("a", href=True): 
             href = a_tag["href"].split("#")[0].strip() 
             full_url = urljoin(base_url, href).strip().lower()
 
@@ -21,3 +31,28 @@ class Extractor:
     def is_internal_link(base_url, url):
         """Checks if a URL belongs to the same domain as the base URL."""
         return urlparse(url).netloc == urlparse(base_url).netloc
+    
+    @staticmethod
+    def extract_email(html_content):
+        email_regex = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+        return re.findall(email_regex, html_content)
+    
+    @staticmethod
+    def extract_names(html_content):
+        soup = BeautifulSoup(html_content, "html.parser")
+        text = soup.get_text()
+
+        ner_results = ner_pipeline(text)
+
+        persons = set()
+        organization = set()
+
+        for ent in ner_results:
+            if ent["entity_group"] == "PER":
+                persons.add(ent["word"])
+            elif ent["entity_group"] == "ORG":
+                organization.add(ent["word"])
+        return {
+            "peron_names": list(persons),
+            "company_names": list(organization)
+        }
--- url.py
+++ url.py
@@ -19,8 +19,8 @@ class URLHandler:
         try:
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
             response = requests.get(url, timeout=30, headers=headers)
-            response.raise_for_status()
-            return response.text
+            return  response.text,response.status_code
+
         except requests.RequestException as e:
             print(f" Error fetching {url}: {e}")
             return None
@@ -31,90 +31,80 @@ class URLHandler:
         cutoff_time = now - timedelta(hours=48)
         process_id = f"crawler_{os.getpid()}"  # Unique identifier for the process
         hostname = socket.gethostname()
+        locked_by = process_id + hostname
 
-        urls_to_lock = list(self.urls_collection.find(
+        urls_to_find = list(self.urls_collection.find(
             {
                 "$or": [
-                    {"last_crawled": {"$lt": cutoff_time}},  # Older than 48 hours
-                    {"last_crawled": {"$exists": False}},
-                    {"status": {"$exists": False}},
-                    {"status": "pending"},  # Always crawl "pending" URLs
-                    {"status": ""},
+                    {"last_crawled": {"$lt": cutoff_time}},  #  Not crawled in 48+ hours
+                    {"last_crawled": {"$exists": False}},    # Never crawled
+                    {"status": {"$exists": False}},          # No status
+                    {"status": {"$in": ["pending", ""]}} 
                 ],
             },
-            {"_id": 1 }  # Fetch only required fields
+            {"_id": 1}  # Fetch only required fields
             ).limit(num_of_urls))
 
-        print(f"Found {len(urls_to_lock)} URLs to lock.")
+        print(f"Found {len(urls_to_find)} URLs to lock.")
 
-        if not urls_to_lock:
+        if not urls_to_find:
             print("No more URLs to process.")
             return []
 
-        url_ids = [url["_id"] for url in urls_to_lock]  # Step 2: Extract IDs to update
+        url_ids = [url["_id"] for url in urls_to_find]  # Step 2: Extract IDs to update
 
         # Lock the URLs by updating their status and adding a process lock
         self.urls_collection.update_many(
         {
             "_id": {"$in": url_ids},
-            "$or": [
-                {"status": {"$exists": False}},  # status doesn't exist
-                {"status": {"$ne": "processing"}},  # status is not "processing"
-                {"status": {"$in": ["", "pending"]}},  # status is either "" or "pending"
-            ],
         },
             {
                 "$set": {
                     "status": "processing",
                     "locked_at": now,
-                    "locked_by": f"{hostname} + {process_id}",
+                    "locked_by": locked_by,
                 }
             }
         )
 
-        urls_to_process = list(self.urls_collection.find(
-        {
-            "$or": [
-                { "_id": {"$in": url_ids}},
-                {"status": {"$exists": False}},
-                {"status": "pending"},
-                {"status": ""}
-            ]
-        },
-        {"_id": 1, "url": 1, "status": 1, "locked_at": 1, "locked_by": 1, "domain_id": 1}
-    ).limit(num_of_urls))
+        locked_url_find = list(self.urls_collection.find(
+            {
+                "$and": [
+                    { "_id": {"$in": url_ids} },
+                    { "status": "processing" },
+                    { "locked_by": locked_by }
+                ]
+            },
 
-        for url_data in urls_to_process:
+            {"_id": 1,"url": 1, "domain_id": 1 }  
+        ))
+
+        for url_data in locked_url_find:
+                url_data["locked_by"] = locked_by
                 self.process_url(url_data)
 
-        self.urls_collection.update_many(
-            {"_id": {"$in": url_ids}},
-            {
-                "$set": {
-                    "status": "complete",
-                    "locked_at": None,
-                    "locked_by": None,
-                    "last_crawled": now,
-                }
-            }
-        )
-
     def process_url(self, url):
-        html_content = self.fetch_html(url["url"])
+        html_content,status_code = self.fetch_html(url["url"])
+        emails = Extractor.extract_email(html_content)
+        names = Extractor.extract_names(html_content)
 
         if html_content:
             extracted_links = Extractor.extract_links(html_content, url["url"])
-            
             self.store_extracted_links(extracted_links,url)  # Store the extracted links in the database
 
               # Update the URL status to 'completed' after processing
-            self.urls_collection.update_one(
+            self.urls_collection.update_many(
                 {"_id": url["_id"], "locked_by": url["locked_by"]},  # Ensure only the same process updates it
                 {"$set": {
                     "last_crawled": datetime.now(UTC),
                     "status": "completed",
                     "locked_at": None,
-                    "locked_by": None
+                    "locked_by": None,
+                    "html_content":html_content,
+                    "Status_code":status_code,
+                    "emails": list(set(emails)),
+                    "person_names":names["person_names"],
+                    "company_names": names["company_names"]
                 }}
             )
             print(f" Successfully processed and completed URL: {url['url']}")
@@ -123,38 +113,16 @@ class URLHandler:
             self.urls_collection.update_one(
                 {"_id": url["_id"], "locked_by": url["locked_by"]},
                 {"$set": {
-                    "status": "pending",
+                    "status": "complete",
+                    "status_code": status_code,
                     "locked_at": None,
                     "locked_by": None
                 }}
             )
-    def save_domain_url(self,url):
-        "store the domain if it's not store"
-        domain = parse_domain(url)
-
-        if not domain:
-            print("Invalid URL: {url}")
-            return False
-        
-        #check if domains exist or not
-        existing = self.db_handler.urls_collection.find_one({"domain": domain})
-        if existing:
-            print(f"Domain {domain} already exist. skipping....")
-            return False
-        
-        #store new domain
-
-        self.db_handler.urls_collection.insert_one({
-            "domain": domain,
-            "original_url": url
-        })
-        print(f"New domain added: {domain}")
-        return True
 
     def store_extracted_links(self, extracted_links,url):
         domain_from_url = parse_domain(url["url"])
         
-
         for link in extracted_links:
             domain_id=0
             if domain_from_url in link:
@@ -163,7 +131,6 @@ class URLHandler:
             link = link.strip() 
             link_hash = generate_md5(link)
             
-            
             self.urls_collection.update_one(
                     {"md5_url":link_hash},   # Find by MD5 hash
                     {
@@ -171,14 +138,12 @@ class URLHandler:
                             "md5_url": link_hash,
                             "url": link,
                             "domain_id": domain_id,
-                            "locked_at": None,
-                            "locked_by": None,
+                            
+                        },
+                        "$set": {
+                           
                         }
                     },
                     upsert=True  # This will insert the link if it's new
                 )
             print(f" Link added: {link}.")
-
-    
-    
-
